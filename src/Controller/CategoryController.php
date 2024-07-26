dcc8a395b78379eb5908cc44dfcc385b5436b2e5
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-07-26 23:44:21 +0200

    feat: add create topic method

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Topic;
 use App\Entity\Category;
+use App\Form\CreateTopicType;
 use App\Repository\CategoryRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route(
     '/category', 
@@ -27,8 +31,35 @@ class CategoryController extends AbstractController
     #[Route('/list/{id}/show', name:'show')]
     public function showCategory(Category $category): Response 
     {
-        return $this->render('category/showCategory.html.twig');
+        return $this->render('category/show.html.twig', [
+            'category' => $category
+        ]);
     }
 
-    
+    #[Route('/list/{id}/show/create-topic', name:'create_topic')]
+    public function createTopic(Category $category, EntityManagerInterface $entityManager, Request $request): Response 
+    {
+
+        $topic = new Topic(); 
+        $user = $this->getUser();
+
+        $form = $this->createForm(CreateTopicType::class, $topic);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $topic = $form->getData();
+            $topic->setUser($user);
+            $topic->setCategory($category);
+
+            $entityManager->persist($topic);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('category_show', ['id' => $category->getId()]);
+        }
+
+        return $this->render('category/create-topic.html.twig', [
+            'form' => $form,
+            'category' => $category
+        ]);
+    }
 }
