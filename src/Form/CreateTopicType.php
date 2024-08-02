f1d3abfacbbcce0bd3715295b2ed71be5802da0f
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-08-02 23:59:53 +0200

    feat: add contraints for title and text

--- src/Form/CreateTopicType.php
+++ src/Form/CreateTopicType.php
@@ -2,22 +2,37 @@
 
 namespace App\Form;
 
-use App\Entity\Category;
 use App\Entity\Topic;
-use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CreateTopicType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('text')
+            ->add('title', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "You can't create Topic without a Title",
+                    ]),
+                ]
+            ])
+            ->add('text', TextareaType::class, [
+                'required' => true,
+                'sanitize_html' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "A Topic need a subject text",
+                    ]),
+                ]
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
