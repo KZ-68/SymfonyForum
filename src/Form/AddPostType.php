9e0295ea7cec1e1e34e6f68ad8b37aefb6bb433a
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-08-03 00:03:24 +0200

    feat: add constraints for text and sanitizer

--- src/Form/AddPostType.php
+++ src/Form/AddPostType.php
@@ -4,17 +4,26 @@ namespace App\Form;
 
 use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class AddPostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('text', TextType::class)
+            ->add('text', TextareaType::class, [
+                'required' => true,
+                'sanitize_html' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "A post can't be submit without a text",
+                    ]),
+                ]
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
