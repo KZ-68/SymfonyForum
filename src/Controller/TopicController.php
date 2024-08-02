8a740e2ae155e7a7ab1c73c149ba36b0a245a09f
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-08-03 00:03:55 +0200

    feat: add condition for user login

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Exception;
 use App\Entity\Post;
 use App\Entity\Topic;
 use App\Form\AddPostType;
@@ -11,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 #[Route(
     '/topic', 
@@ -34,21 +36,24 @@ class TopicController extends AbstractController
     {
         $post = new Post(); 
         $user = $this->getUser();
+        if($user == NULL) {
+            $form = $this->createForm(AddPostType::class, $post);
 
-        $form = $this->createForm(AddPostType::class, $post);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $post = $form->getData();
+                $post->setUser($user);
+                $post->setTopic($topic);
 
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $post = $form->getData();
-            $post->setUser($user);
-            $post->setTopic($topic);
+                $entityManager->persist($post);
+                $entityManager->flush();
 
-            $entityManager->persist($post);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('topic_show', ['id' => $topic->getId()]);
+                return $this->redirectToRoute('topic_show', ['id' => $topic->getId()]);
+            }
+        } else {
+            throw new AuthenticationException("You have to login in order to post in the forum", 1);
         }
-
+        
         return $this->render('topic/add-post.html.twig', [
             'form' => $form,
             'topic' => $topic
