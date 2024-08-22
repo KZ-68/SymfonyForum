5d84aa25741d21b1c776e19edc3896c7934d1327
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-08-23 01:55:43 +0200

    feat: add topics list, edit and remove topic methods

--- src/Controller/TopicController.php
+++ src/Controller/TopicController.php
@@ -6,14 +6,16 @@ use Exception;
 use App\Entity\Post;
 use App\Entity\Topic;
 use App\Form\AddPostType;
+use App\Form\EditTopicType;
 use App\Repository\PostRepository;
+use App\Repository\TopicRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route(
     '/topic', 
@@ -22,13 +24,24 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 #[IsGranted('ROLE_USER')]
 class TopicController extends AbstractController
 {
+    #[Route('/list', name: 'list')]
+    #[IsGranted('ROLE_USER')]
+    public function index(TopicRepository $topicRepository): Response
+    {
+        $topics = $topicRepository->findAll();
+
+        return $this->render('topic/index.html.twig', [
+            'topics' => $topics,
+        ]);
+    }
+
     #[Route('/{id}/show', name: 'show')]
     #[IsGranted('ROLE_USER')]
-    public function index(Topic $topic, PostRepository $postRepository): Response
+    public function show(Topic $topic, PostRepository $postRepository): Response
     {
         $posts = $postRepository->findBy(['topic' => $topic]);
 
-        return $this->render('topic/index.html.twig', [
+        return $this->render('topic/show.html.twig', [
             'topic' => $topic,
             'posts' => $posts
         ]);
@@ -62,5 +75,37 @@ class TopicController extends AbstractController
             'form' => $form,
             'topic' => $topic
         ]);
+    }
+
+    #[Route('/list/{id}/edit-topic', name:'edit')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function edittopic(Topic $topic, EntityManagerInterface $entityManager, Request $request): Response 
+    {
+        $form = $this->createForm(EditTopicType::class, $topic);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $topic = $form->getData();
+
+            $entityManager->persist($topic);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('topic_list');
+        }
+
+        return $this->render('topic/edit-topic.html.twig', [
+            'form' => $form,
+            'topic' => $topic
+        ]);
+    }
+
+    #[Route('/list/{id}/remove-topic', name: 'remove')]
+    #[IsGranted("ROLE_ADMIN")]
+    public function removetopic(Topic $topic, EntityManagerInterface $entityManager) {
+        
+        $entityManager->remove($topic);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('topic_list');
     }
 }
