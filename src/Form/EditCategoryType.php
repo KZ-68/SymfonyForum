097d838d494f15a7bce9cfbdf3794cb4d4daeeb6
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-08-23 01:58:35 +0200

    feat: add noBlank constraint validation for name

--- src/Form/EditCategoryType.php
+++ src/Form/EditCategoryType.php
@@ -4,17 +4,25 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EditCategoryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class)
+            ->add('name', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => "You can't create or edit an category without a name",
+                    ]),
+                ]
+            ])
             ->add('confirm', SubmitType::class)
         ;
     }
