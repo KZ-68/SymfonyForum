c2ef4eba79e3739349e62da799e3756e2405b895
Kevin ZITNIK <zitnik.kevin@gmail.com>
2024-08-29 23:59:13 +0200

    feat: add permissions for this controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,13 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+#[Route(
+    '/admin', 
+    name: 'admin_'
+)]
+#[IsGranted('ROLE_ADMIN')]
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
+    #[Route('/panel', name: 'panel')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', [
